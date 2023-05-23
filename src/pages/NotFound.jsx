127e4a98b116bbe2899a5f4f1194036162c6e47b
nikolicsmilan <milan.nikolics@weborigo.eu>
2023-05-23 14:42:02 +0200

    add 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
+import React from "react";
+import { useTranslation } from "react-i18next";
 const NotFound = () => {
+    const { t } = useTranslation();
   return (
-    <div>NotFound</div>
-  )
-}
+    <div className="flex items-center justify-center h-screen text-primary text-xl mx-2">
+   {t('errror404')}
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
