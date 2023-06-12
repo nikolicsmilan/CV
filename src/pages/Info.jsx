fc43791af19cf2f2040f3a16c0a850465ea4576f
nikolicsmilan <nikolicsmilan@gmail.com>
2023-06-12 22:34:34 +0200

    Typing Animation progress

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -5,20 +5,102 @@ import {
   myOnSnapShotUpdateVisit,
 } from "../firebase/Firestore";
 import { MyInfoContext } from "../context/InfoContext";
+import TypingAnimation from "../components/info/TypingAnimation";
 
 const Info = () => {
   const { visit, setVisit, latitude, longitude, ipAddress, browserInfo } =
     MyInfoContext();
-  const [showSecondText, setShowSecondText] = useState(false);
-  const [showThirdText, setShowThirdText] = useState(false);
-  const [showFourthText, setShowFourthText] = useState(false);
   const { t } = useTranslation();
   const {
     browser: { name: browserName },
     os: { name: osName },
     device: { type: deviceType },
   } = browserInfo;
-  console.log("browserName", browserName, osName, deviceType);
+
+  useEffect(() => {
+    const unsubscribe = myOnSnapshotVisited(setVisit);
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const shouldShowText =
+    visit || ipAddress || longitude || latitude || browserName || osName || deviceType;
+
+  const getTextContent = () => {
+    if (shouldShowText) {
+      let content = "";
+      if (visit) {
+        content += `${t("Infom.visited", { visit })} `;
+      }
+      if (longitude && latitude) {
+        content += `${t("Infom.gps", { longitude, latitude })} `;
+      }
+      if (ipAddress) {
+        content += `${t("Infom.ip", { ipAddress })} `;
+      }
+      if (browserName) {
+        content += `${t("Infom.browser", { browserName })} `;
+      }
+      if (osName) {
+        content += `${t("Infom.os", { osName })} `;
+      }
+      if (deviceType) {
+        content += `${t("Infom.devicetype", { deviceType })} `;
+      }
+      return content.trim(); // Trim any extra whitespace
+    }
+    return "No information available";
+  };
+
+  return (
+    <div className="text-primary text-xl mx-2">
+      <TypingAnimation customcontent={getTextContent()} />
+    </div>
+  );
+};
+
+export default Info;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import React, { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import {
+  myOnSnapshotVisited,
+  myOnSnapShotUpdateVisit,
+} from "../firebase/Firestore";
+import { MyInfoContext } from "../context/InfoContext";
+import TypingAnimation from "../components/info/TypingAnimation";
+
+const Info = () => {
+  const { visit, setVisit, latitude, longitude, ipAddress, browserInfo } =
+    MyInfoContext();
+  const { t } = useTranslation();
+  const {
+    browser: { name: browserName },
+    os: { name: osName },
+    device: { type: deviceType },
+  } = browserInfo;
+ // console.log("browserName", browserName, osName, deviceType);
   //This here and not context cause only run when this page is load
   useEffect(() => {
     const unsubscribe = myOnSnapshotVisited(setVisit);
@@ -27,68 +109,21 @@ const Info = () => {
     };
   }, []);
 
-  useEffect(() => {
-    myOnSnapShotUpdateVisit();
-    // Show the second text after a delay of 2 seconds
-    const timeout = setTimeout(() => {
-      setShowSecondText(true);
-    }, 2000);
-
-    // Show the third text after a delay of 4 seconds (2 seconds after the second text)
-    const timeout2 = setTimeout(() => {
-      setShowThirdText(true);
-    }, 4000);
-
-    const timeout3 = setTimeout(() => {
-      setShowFourthText(true);
-    }, 6000);
-
-    return () => {
-      clearTimeout(timeout);
-      clearTimeout(timeout2);
-      clearTimeout(timeout3);
-    };
-  }, []);
-
   return (
-    <div className="flex items-center justify-center h-screen text-primary text-xl mx-2">
-      <div className="text-info ">
-        <span className="typing-animation ">
-          {t("Infom.visited", { visit })}
-        </span>
-        {showSecondText && longitude && latitude && (
-          <>
-            <br></br>
-            <span className="typing-animation">
-              {t("Infom.gps", { longitude, latitude })}
-            </span>
-          </>
-        )}{" "}
-        {showThirdText && ipAddress && (
-          <>
-            <br></br>
-            <span className="typing-animation">
-              {" "}
-              {t("Infom.ip", { ipAddress })}
-            </span>
-          </>
-        )}
-        {showFourthText && browserName && osName && (
-          <>
-            <br></br>
-            <span className="typing-animation">
-              {" "}
-              {t("Infom.browser", { browserName, osName })}
-            </span>
-          </>
-        )}
-      </div>
+    <div className="text-primary text-xl mx-2">
+      <TypingAnimation
+        customcontent={`${t("Infom.visited", { visit })} ${t("Infom.ip", {
+          ipAddress,
+        })} ${t("Infom.gps", { longitude, latitude })} ${t("Infom.browser", {
+          browserName,
+          osName, deviceType
+        })}`}
+      />
     </div>
   );
 };
 
-export default Info;
-
+export default Info;*/
 
 /* GPS
         Weather
