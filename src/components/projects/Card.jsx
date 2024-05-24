736092848f195479a8f18c381a922cdcedd157a9
nikolicsmilan <nikolicsmilan@gmail.com>
2024-05-24 14:15:18 +0200

    image and icon upgrade in projects page

--- src/components/projects/Card.jsx
+++ src/components/projects/Card.jsx
@@ -5,6 +5,106 @@ import { TbWorldWww } from "react-icons/tb";
 import { useTranslation } from "react-i18next";
 import nasa from "../../assets/nasa_project.JPG";
 
+const Card = ({
+  title,
+  description,
+  weblink,
+  gitlink,
+  background,
+  keyword,
+  image,
+  stack,
+}) => {
+  const { t } = useTranslation();
+  function getColor(title) {
+    switch (title) {
+      case "React":
+        return "text-sky-400";
+      case "Js":
+        return "text-yellow-200";
+      case "Node":
+        return "text-green-400 ";
+      case "tailwind":
+        return "text-blue-300";
+      case "HTML":
+        return "text-orange-400 ";
+      case "CSS":
+        return "text-blue-400 ";
+      case "Firebase":
+        return "text-amber-400 ";
+      default:
+        return "text-sky-500"; // Alapértelmezett szín, ha nincs meghatározott címke
+    }
+  }
+
+  return (
+    <div
+      className={`text-dark  relative ${background} p-0 pt-0 rounded border-2 space:shadow-2xl  space:border-sky-400 w-80 `}
+    >
+      <img src={image} className="w-full h-60 rounded" />
+      <h1 className="text-lg rounded-full  text-white p-2">{title}</h1>
+      <div className="h-36">
+        <p className="text-truncate  overflow-hidden text-white text-base p-2">
+          {t(`Project.${description}`)}
+        </p>
+      </div>
+
+      <div className="mt-0 border-0 border-red-400 h-12 p-1">
+        <span className="text-3xl font-semibold ">
+          {gitlink && (
+            <a href={gitlink} target="_blank" rel="noreferrer">
+              <FaGithub className="text-purple-400 hover:text-purple-600 top-[245px] right-[50px] absolute" />
+            </a>
+          )}
+          {weblink && (
+            <a href={weblink} target="_blank" rel="noreferrer">
+              <TbWorldWww
+                className={`text-red-400 hover:text-red-600 top-[245px] right-[-40px]  absolute ${
+                  gitlink ? "mx-12 " : ""
+                }`}
+              />
+            </a>
+          )}
+        </span>
+        <div className="left-0 absolute flex px-2">
+          {stack?.map((item) => (
+            <div className={`text-3xl ${getColor(item.name)}`}>{item.icon}</div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  weblink: PropTypes.string,
+  gitlink: PropTypes.string,
+  background: PropTypes.string.isRequired,
+  keyword: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Card;
+/*
+    <div className="bottom-12 absolute text-white border-0 border-amber-400 p-1">
+          #
+          {keyword.map((item, index) => (
+            <span key={index} className="text-md  ">
+              {item},{" "}
+            </span>
+          ))}
+        </div>
+*/
+/*
+
+import React from "react";
+import PropTypes from "prop-types";
+import { FaGithub } from "react-icons/fa";
+import { TbWorldWww } from "react-icons/tb";
+import { useTranslation } from "react-i18next";
+import nasa from "../../assets/nasa_project.JPG";
+
 
 const Card = ({
   title,
@@ -13,13 +113,15 @@ const Card = ({
   gitlink,
   background,
   keyword,
+  image,
+  stack
 }) => {
   const { t } = useTranslation();
   return (
     <div
-      className={`text-dark  relative ${background} p-0 pt-0 rounded-2xl border-2  border-sky-400 w-80 shadow-2xl`}
+      className={`text-dark  relative ${background} p-0 pt-0 rounded border-2  space:border-sky-400 w-80 shadow-2xl`}
     >
-      <img src={nasa} className="w-full" />
+      <img src={image} className="w-full h-60 rounded" />
       <h1 className="text-lg rounded-full  text-white p-2">
         {title}
       </h1>
@@ -29,7 +131,7 @@ const Card = ({
         </p>
       </div>
 
-      <div className="mt-0 border-2 border-red-400 h-12">
+      <div className="mt-0 border-0 border-red-400 h-12 p-1">
         <span className="text-3xl font-semibold ">
           {gitlink && (
             <a href={gitlink} target="_blank" rel="noreferrer">
@@ -44,8 +146,16 @@ const Card = ({
             </a>
           )}
         </span>
+        <div className="right-0 absolute">
+          {stack?.map((item)=>(
+            <div>
+{item.name}
 
-        <div className="bottom-12 absolute text-white border-0 border-amber-400">
+            </div>
+          ))}
+        </div>
+
+        <div className="bottom-12 absolute text-white border-0 border-amber-400 p-1">
           #
           {keyword.map((item, index) => (
             <span key={index} className="text-md  ">
@@ -68,3 +178,4 @@ Card.propTypes = {
 };
 
 export default Card;
+*/
--- src/data/projectsdata.js
+++ src/data/projectsdata.js
@@ -1,51 +1,100 @@
+import calendar from "../assets/calendar_project.jpg";
+import nasa from "../assets/nasa_project.JPG";
+import cv from "../assets/identitycard10.PNG";
+import sm from "../assets/sm_project.JPG";
+import {
+  FaReact,
+  FaBootstrap,
+  FaJs,
+  FaCss3Alt,
+  FaHtml5,
+  FaNodeJs,
+  FaGit,
+  FaNpm,
+  //  FaInfoCircle,
+  // FaMapMarkerAlt,
+} from "react-icons/fa";
+import { SiTailwindcss, SiFirebase } from "react-icons/si";
 export const projects = [
-
   {
     title: "Nasa interface",
-    description:
-      "description4",
+    description: "description4",
     textColor: "",
     backgroundColor: "bg-gradient",
     keyword: ["React", "context", "tailwind", "firebase"],
     gitlink: "https://github.com/nikolicsmilan/nasa",
     weblink: "https://nasa-ab2f1.web.app/",
+    image: nasa,
+    stack: [
+      { name: "React", icon: <FaReact /> },
+      { name: "Js", icon: <FaJs /> },
+      { name: "CSS", icon: <FaCss3Alt /> },
+      { name: "HTML", icon: <FaHtml5 /> },
+      { name: "Tailwind", icon: <SiTailwindcss /> },
+      { name: "Node", icon: <FaNodeJs /> },
+      { name: "Firebase", icon: <SiFirebase /> },
+    ],
   },
   {
     title: "Fullstack Calendar App",
-    description:
-      "description1",
+    description: "description1",
     textColor: "",
     backgroundColor: "bg-gradient",
     keyword: ["React", "context", "module css", "node"],
     gitlink:
       "https://github.com/nikolicsmilan/Full-Stack-Callendar/tree/master",
     weblink: "",
+    image: calendar,
+    stack: [
+      { name: "React", icon: <FaReact /> },
+      { name: "Js", icon: <FaJs /> },
+      { name: "CSS", icon: <FaCss3Alt /> },
+      { name: "HTML", icon: <FaHtml5 /> },
+      { name: "Tailwind", icon: <SiTailwindcss /> },
+      { name: "Node", icon: <FaNodeJs /> },
+      { name: "Firebase", icon: <SiFirebase /> },
+    ],
   },
   {
     title: "CV",
-    description:
-      "description2",
+    description: "description2",
     textColor: "",
     backgroundColor: "bg-gradient",
     keyword: ["React", "context", "tailwind, firebase"],
-    gitlink:
-      "https://github.com/nikolicsmilan/CV",
-    weblink: "",
+    gitlink: "https://github.com/nikolicsmilan/CV",
+    weblink: "https://nikolicsmilan-cv.web.app/",
+    image: cv,
+    stack: [
+      { name: "React", icon: <FaReact /> },
+      { name: "Js", icon: <FaJs /> },
+      { name: "CSS", icon: <FaCss3Alt /> },
+      { name: "HTML", icon: <FaHtml5 /> },
+      { name: "Tailwind", icon: <SiTailwindcss /> },
+      { name: "Node", icon: <FaNodeJs /> },
+      { name: "Firebase", icon: <SiFirebase /> },
+    ],
   },
 
-
   {
-    title: "Image viewer",
-    description:
-      "description3",
+    title: "Webshop furniture",
+    description: "description3",
     textColor: "",
     backgroundColor: "bg-gradient",
     keyword: ["React", "context", "tailwind", "firebase"],
     gitlink: "https://github.com/nikolicsmilan/SM",
     weblink: "",
+    image: sm,
+    stack: [
+      { name: "React", icon: <FaReact /> },
+      { name: "Js", icon: <FaJs /> },
+      { name: "CSS", icon: <FaCss3Alt /> },
+      { name: "HTML", icon: <FaHtml5 /> },
+      { name: "Tailwind", icon: <SiTailwindcss /> },
+      { name: "Node", icon: <FaNodeJs /> },
+      { name: "Firebase", icon: <SiFirebase /> },
+    ],
   },
 ];
-
 
 /*
 {
